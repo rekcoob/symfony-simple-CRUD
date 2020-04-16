c35ff5eb0daa397472babac4e9c88d8194b44cec
ImDakoty <shanoun@webpack.sk>
2020-04-16 15:31:17 +0200

    first route

---
+++ ReadMe.txt
@@ -0,0 +1,12 @@
+# check if composer is installed
+Composer
+
+# create projects without binary - server,...
+composer create-project symfony/skeleton my_project_name
+
+
+#create routes anotations
+composer require annotations
+
+#bundle that allows us to use GET, POST, .. methods - installed with anotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
---
+++ src/Controller/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class PostController
+{
+     /**
+      * @Route("/")
+      */
+    public function index()
+    {       
+        return new Response(
+            '<html><body>Hello World</body></html>'
+        );
+    }
+}
